102ed69c1145e561c4c830b795682f05a3e1a2c0
Manoel Cardoso da Silveira Neto <manoel.neto@unimednoroesters.com.br>
2025-08-11 17:28:35 -0300

    feat (auth): implementada flash message na validação do registro to register

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,7 @@
+const User = require('../models/ManoelUser')
+
+const bcrypt = require('bcryptjs')
+
 module.exports = class AuthController {
 
     static login(req, res){
@@ -8,4 +12,25 @@ module.exports = class AuthController {
         res.render('auth/register');
     }
 
+    static async registerPost(req, res){
+        const { username, email, password, confirm_password } = req.body;
+
+        // password match validation
+        if (password !== confirm_password) {
+            req.flash('message', 'A senha e a confirmação não se equivalem, tente novamente')
+            return res.render('auth/register');
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
+        await user.save();
+
+        res.redirect('/login');
+    }
 }
